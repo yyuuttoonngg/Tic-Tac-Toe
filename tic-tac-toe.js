f25e5470836986d9fafb041c34fb4b7d1aac7e94
Tong Yu <rachieyu@gmail.com>
2018-07-04 00:02:12 +1000

    created game logic and basic html css for tic-tac-toe project

---
+++ tic-tac-toe.js
@@ -0,0 +1,83 @@
+console.log('tic-tac-toe');
+
+var boxes = document.querySelectorAll('.box');
+var counter = 0;
+var winnerArray = ['123','147','456','258','789','369','357','159'];
+var score ={
+    player1:0,
+    player2:0,
+    totalRounds:0
+}
+
+
+boxes.forEach(function(box){
+    box.addEventListener('click',clickedBox);
+    displayCurrentPlayer();
+});
+
+document.querySelector('button').addEventListener('click',resetGame);
+
+function clickedBox(event){
+    event.target.removeEventListener('click',clickedBox);
+    if (counter%2===0){
+        event.target.classList.add('player1');
+    } else {
+        event.target.classList.add('player2');
+    }
+    counter +=1; 
+    checkWiner("player1");
+    checkWiner("player2");
+
+    if (counter===9&&!checkWiner("player1")&&!checkWiner("player2")){
+        document.querySelector('.result').textContent = 'nobody won'
+        document.querySelector('.hidden').classList.add('display');
+    }
+
+    displayCurrentPlayer();
+ 
+}
+
+function checkWiner(player){
+    var playerArray =[];
+    document.querySelectorAll("."+player).forEach(function(a){
+        playerArray.push(a.textContent);
+    })
+    var string =playerArray.join('');
+    for (i=0; i<winnerArray.length; i++){
+        if (string.slice(0,3) === winnerArray[i] || 
+        string.slice(1)===winnerArray[i] || 
+        (string.slice(0,1)+string.slice(2))===winnerArray[i] || 
+        (string.slice(0,2)+string.slice(3))===winnerArray[i] ||
+        string ==='12569'|| string==='23457' || string==='35678'||string ==='14589'){
+            document.querySelector('.result').textContent = 'winner is ' + player;
+            document.querySelector('#'+player).textContent = score[player];
+            document.querySelectorAll('.box').forEach(function(a){
+                a.removeEventListener('click',clickedBox);
+            })
+            document.querySelector('.hidden').classList.add('display');
+            score[player] +=1;
+            return true;
+        }
+    }
+}
+
+function resetGame(){
+    score.totalRounds +=1;
+    
+    boxes.forEach(function(box){
+        box.className = 'box';
+        box.addEventListener('click',clickedBox);
+    });
+    document.querySelector('.hidden').classList.remove('display');
+    counter = score.totalRounds%2;
+    displayCurrentPlayer();
+}
+
+function displayCurrentPlayer(){
+    if(counter%2===0){
+        document.querySelector('.currentplayer').textContent = 'playing: player1'
+    } else {
+        document.querySelector('.currentplayer').textContent = 'playing: player2'
+        
+    }
+}
